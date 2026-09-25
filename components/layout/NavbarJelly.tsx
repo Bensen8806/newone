d4b3bee7da1669e574af17d2f2bacec4c781c32f
basithalikp <sithukp50@gmail.com>
2026-09-25 10:48:45 +0530

    Bug fixes on merge

--- components/layout/NavbarJelly.tsx
+++ components/layout/NavbarJelly.tsx
@@ -74,15 +74,13 @@ export default function NavbarJelly({ user, dashboardLink, showMap, clubLogo }: 
         )}
       </div>
 
-      <div className="fixed top-6 left-0 w-full z-50 flex justify-center pointer-events-none px-6">
-        <div className="pointer-events-auto">
-          <JellyRadio 
-            items={items}
-            value={activeValue}
-            onChange={(val) => router.push(val)}
-          />
-        </div>
+      <div className="pointer-events-auto">
+        <JellyRadio 
+          items={items}
+          value={activeValue}
+          onChange={(val) => router.push(val)}
+        />
       </div>
-    </>
+    </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,9 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-      }
+      },
+      {
+        protocol: 'https',
         hostname: '**',
       },
       {
